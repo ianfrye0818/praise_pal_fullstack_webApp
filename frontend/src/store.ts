be4095f4960f72309eed2ea4625c4b6b5bfa05d8
Ian <ian_frye@outlook.com>
2024-06-06 16:57:04 -0400

    started working on frontend

---
+++ frontend/src/store.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { create } from 'zustand';
+
+interface BearState {
+  bears: number;
+  increase: (by: number) => void;
+}
+
+const useBearStore = create<BearState>()((set) => ({
+  bears: 0,
+  increase: (by) => set((state) => ({ bears: state.bears + by })),
+}));
